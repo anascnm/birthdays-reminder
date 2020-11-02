c10ed74a71734b7dccc20da6782fbbfa6d6c294b
Anastasia Mendoza <nidesignstt@gmail.com>
2020-11-02 15:20:15 -0400

    project finished

---
+++ src/data.js
@@ -0,0 +1,39 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+      id: 1,
+      name: 'Cynda Yates',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/nidesigns/image/upload/v1604342565/birthday-app%20images/ethan-haddox-NTjSR3zYpsY-unsplash_fjxq39.jpg',
+    },
+    {
+      id: 2,
+      name: 'Oladimeji Ajegbile',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/nidesigns/image/upload/v1604342534/birthday-app%20images/pexels-oladimeji-ajegbile-3556533_u3qwpf.jpg',
+    },
+    {
+      id: 3,
+      name: 'Dami Adebayo',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/nidesigns/image/upload/v1604342489/birthday-app%20images/dami-adebayo-k6aQzmIbR1s-unsplash_hl0mmi.jpg',
+    },
+    {
+      id: 4,
+      name: 'Jessica Felicio',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/nidesigns/image/upload/v1604342481/birthday-app%20images/jessica-felicio-_cvwXhGqG-o-unsplash_ua4ghj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/nidesigns/image/upload/v1604342540/birthday-app%20images/arvin-keynes-IPETsB4dcCs-unsplash_f8o4jn.jpg',
+    },
+  ];
+  
